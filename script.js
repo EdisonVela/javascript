1826ce268a5bb3487d3d2a9667c1ea3b65406f3e
Edison Vela <evelag@est.ups.edu.ec>
2025-06-09 16:26:48 -0500

    Alert

---
+++ script.js
@@ -0,0 +1,11 @@
+//Funcion para mostrar un cuadro de dialogo emergente 
+function mostrarMensaje(){
+    alert("Hola Mundo :)");
+}
+
+//Variables
+function imprimirvariables(){
+    let nombre = "Edison"
+    let edad = "22"
+    
+}
