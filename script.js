dd61eac4b558b2780d76b45f929a78ec07e05bf5
Edison Vela <evelag@est.ups.edu.ec>
2025-06-09 16:53:17 -0500

    Arreglos Bucles y condicional

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
-//Funcion para mostrar un cuadro de dialogo emergente 
-function mostrarMensaje(){
+// Función para mostrar un cuadro de diálogo emergente
+function mostrarMensaje() {
     alert("Hola Mundo :)");
 }
 
-//Variables
-function imprimirvariables(){
-    let nombre = "Edison"
-    let edad = "22"
-    console.log("nombre:" , nombre);
-    console.log("edad: " , edad);
-//Forma moderna de concatenar cadenas
-    console.log(`nombre : ${nombre}, edad: ${edad}`);
-    console.error("Error: bla bla bla");
-    console.warn("Advertencia: x ");
+// VARIABLES
+function imprimirVariables() {
+    let nombre = "Marcelo";
+    let edad = "22";
 
-    function arreglos(){
-        let frutas = ["manzana" , "pera" , "banana"];
-        for(let f of fruta) {
-        }
+    console.log("nombre:", nombre);
+    console.log("edad:", edad);
+
+    // Forma correcta de concatenar cadenas usando template literals
+    console.log(`nombre: ${nombre}, edad: ${edad}`);
+
+    console.error("Error: BLA BLA BLA");
+    console.warn("Advertencia: x x x");
+}
+function arreglos(){
+    let frutas = ['Manzana', 'Pera', 'Banana'];
+    //BUCLE
+    for(let f of frutas){
+        console.log(f)
+    }
+    //CONDICIONALES
+    if(frutas[1] ==='pera'){
+        console.log("La segunda fruta es una pera");
+    } else{
+        console.log('NO es una PERA');
     }
 }
+
+
